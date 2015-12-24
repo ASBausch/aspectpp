2b338d0b35f41ea0fd4e55234dfca3e5ca9868b6
Amanda Bausch <manadab@me.com>
2015-12-24 16:55:22 -0600

    tab menu added

---
+++ public/javascripts/tabMenu.js
@@ -0,0 +1,17 @@
+/**
+ * Created by manadab on 12/24/15.
+ */
+
+jQuery(document).ready(function() {
+    jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).fadeIn(1200).siblings().hide();
+
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+    });
+});
